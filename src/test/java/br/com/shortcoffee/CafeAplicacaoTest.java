f51e581720a59ceecfb15dd13793eb64d2d84cac
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-06-02 21:56:18 -0300

    teste3

--- src/test/java/br/com/shortcoffee/CafeAplicacaoTest.java
+++ src/test/java/br/com/shortcoffee/CafeAplicacaoTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,12 +27,33 @@ public class CafeAplicacaoTest {
     private MockMvc mockMvc;
 
     @Test
-    public void criandoCafeicultor() throws Exception {
+    public void criandoCafeicultor1() throws Exception { // Success
         Cafeicultor cafeicultor = Cafeicultor.builder().cpf("123456").email("moises@gmail.com").nome("Moises").telefone("4002-8922").build();
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(cafeicultor);
         mockMvc.perform(MockMvcRequestBuilders.post("/api/coffee/cafeicultor")
                 .content(json).contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void criandoCafeicultor2() throws Exception { // Fail
+        Cafeicultor cafeicultor = Cafeicultor.builder().cpf("123456").email("moises@gmail.com").nome("Moises").telefone("4002-8922").build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(cafeicultor);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/coffee/cafeicultor")
+                .content(json).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(result -> assertEquals("Cpf já cadastrado", result.getResolvedException().getMessage()));
+    }
+
+    @Test
+    public void criandoCafeicultor3() throws Exception { // Email faill
+        Cafeicultor cafeicultor = Cafeicultor.builder().cpf("456789").email("moises@gmail.com").nome("Moises").telefone("4002-8922").build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(cafeicultor);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/coffee/cafeicultor")
+                .content(json).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     }
 }
